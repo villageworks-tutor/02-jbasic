c850e4d333f329809e0109c05e0f51aa1f71cbe4
villageworks <villageworks@gmail.com>
2023-07-09 16:42:26 +0900

    課題JB70

---
+++ src/kadai7/Book.java
@@ -0,0 +1,45 @@
+package kadai7;
+
+/**
+ * 課題JB70 Step-5
+ */
+public class Book extends Item {
+	
+	/**
+	 * フィールド
+	 */
+	private String author; // 著者
+
+	/**
+	 * デフォルトコンストラクタ
+	 */
+	public Book() {}
+
+	/**
+	 * コンストラクタ
+	 * @param name   商品名
+	 * @param price  価格
+	 * @param author 著者
+	 */
+	public Book(String name, int price, String author) {
+		super(name, price);
+		this.author = author;
+	}
+	
+	/** アクセサメソッド群 */
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public void showInfo() {
+		super.showInfo();
+		System.out.println("著者：" + this.author);
+	}
+	
+}
